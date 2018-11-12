ce41a67062e277838ae236b838b973c75f083e53
LeeKangJin <ranian129@gmail.com>
2018-11-12 17:24:29 +0900

    create first spring project.

---
+++ src/main/java/kr/fruitful/life/basic/controller/SampleController.java
@@ -0,0 +1,21 @@
+package kr.fruitful.life.basic.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ranian129@gmail.com on 2018. 11. 12.
+ * Blog : http://76jin.tistory.com
+ * Github : http://github.com/76jin
+ */
+@Log4j
+@Controller
+public class SampleController {
+
+    @GetMapping("/doA")
+    public void doA() {
+        log.info("## doA called....");
+        log.info("-----------------");
+    }
+}
